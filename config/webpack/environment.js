9e46fc5c1db73321691d7c0196973ec310af049c
Abehuerta <abrhuerta@gmail.com>
2021-02-10 15:02:33 -0600

    removed precomile on initialization

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,6 +2,11 @@ const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
 
+// resolve-url-loader must be used before sass-loader
+environment.loaders.get('sass').use.splice(-1, 0, {
+  loader: 'resolve-url-loader'
+})
+
 environment.plugins.append('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
